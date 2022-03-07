131af51dfe9664901a5c865f55828c174ad4eb95
kungreat2021 <kungreat2021@outlook.com>
2022-03-07 11:16:57 +0800

    认证页面初始化

---
+++ web/js/forget.js
@@ -0,0 +1,23 @@
+//窗体大小改变事件
+window.onresize=function(event) {
+    heightCenter();
+};
+function heightCenter() {
+    var currentHeight = document.documentElement.clientHeight;
+    // var currentWidth = document.documentElement.clientWidth;
+    if(currentHeight < 360){
+        document.getElementById("container-forget").style.top='0px';
+    }else{
+        document.getElementById("container-forget").style.top=currentHeight / 2 - 180+'px';
+    }
+};
+heightCenter();
+document.getElementById("subUser").addEventListener("mousedown", function( event ) {
+    // 在被点击的div内显示当前被点击次数
+    //  event.target.textContent = "click count: " + event.detail;
+    event.currentTarget.style.transform="scale(0.9)";
+}, false);
+document.getElementById("subUser").addEventListener("mouseup", function( event ) {
+    // 在被点击的div内显示当前被点击次数
+    event.currentTarget.style.transform="scale(1)";
+}, false);
