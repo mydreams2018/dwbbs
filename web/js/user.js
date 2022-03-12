a9b95f32ef8f9048ec4c836b57e1082c30b5b719
kungreat2021 <kungreat2021@outlook.com>
2022-03-12 20:17:39 +0800

    添加用户完成

--- web/js/user.js
+++ web/js/user.js
@@ -1,12 +1,19 @@
 //窗体大小改变事件
 window.onresize=function(event) {
     heightCenterLength();
+    fixedBottom();
 };
 function heightCenterLength() {
     var currentHeight = document.documentElement.clientHeight;
-    console.log(currentHeight);
     document.getElementById("userMain").style.height=currentHeight-6+"px";
 };
+function fixedBottom() {
+    //可以获得当前元素的上下左右距离视口的距离...有大用了
+    let width = document.getElementById("addchats").getBoundingClientRect().width;
+    let left = document.getElementById("addchats").getBoundingClientRect().left;
+    document.getElementById("fixedBottom").style.left=left+"px";
+    document.getElementById("fixedBottom").style.width=width+"px";
+}
 heightCenterLength();
 
 function searchColor(num) {
@@ -16,3 +23,11 @@ function searchColor(num) {
         document.getElementById("bi-search").style.color="black";
     }
 }
+function searchColoradd(num) {
+    if(num){
+        document.getElementById("add-bi-search").style.color="tomato";
+    }else {
+        document.getElementById("add-bi-search").style.color="black";
+    }
+}
+fixedBottom();
