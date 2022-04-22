40e83e2359223ba845fa0ed0c4139954f253e77c
kungreat2021 <kungreat2021@outlook.com>
2022-04-22 11:51:03 +0800

    用户好友页面优化

--- web/js/userSocket.js
+++ web/js/userSocket.js
@@ -137,6 +137,7 @@ socket.addEventListener('message', function (event) {
                         <li>
                             <h3>${groupByToMapvalue.nikeName}</h3>
                             <span>${groupByToMapvalue.registerTime}</span>
+                             <p>这里是这个人的描述信息...</p>
                         </li>
                         <li>
                             <i data-id="editFixed1${tempuuid}" data-nk="${groupByToMapvalue.nikeName}" class="bi-three-dots-vertical"></i>
